8bd5d3466261d08a926601d926f380e6c6a1dd39
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2026-04-05 13:15:19 +0900

    feat: AD rules for ExtractDiag, EmbedDiag (inverse pair)

--- tenferro-ops/src/ad/diagonal.rs
+++ tenferro-ops/src/ad/diagonal.rs
@@ -1,40 +1,92 @@
 use computegraph::fragment::FragmentBuilder;
-use computegraph::types::LocalValId;
+use computegraph::types::{LocalValId, OpMode, ValRef};
 
 use crate::std_tensor_op::StdTensorOp;
 
 pub fn linearize_extract_diag(
-    _builder: &mut FragmentBuilder<StdTensorOp>,
-    _tangent_in: &[Option<LocalValId>],
-    _axis_a: usize,
-    _axis_b: usize,
+    builder: &mut FragmentBuilder<StdTensorOp>,
+    tangent_in: &[Option<LocalValId>],
+    axis_a: usize,
+    axis_b: usize,
 ) -> Vec<Option<LocalValId>> {
-    todo!()
+    // TODO: ExtractDiag/EmbedDiag will be replaced by Gather/Scatter (Tier-2)
+    match tangent_in[0] {
+        Some(dx) => {
+            let out = builder.add_op(
+                StdTensorOp::ExtractDiag { axis_a, axis_b },
+                vec![ValRef::Local(dx)],
+                OpMode::Linear {
+                    active_mask: vec![true],
+                },
+            );
+            vec![Some(out[0])]
+        }
+        None => vec![None],
+    }
 }
 
 pub fn linearize_embed_diag(
-    _builder: &mut FragmentBuilder<StdTensorOp>,
-    _tangent_in: &[Option<LocalValId>],
-    _axis_a: usize,
-    _axis_b: usize,
+    builder: &mut FragmentBuilder<StdTensorOp>,
+    tangent_in: &[Option<LocalValId>],
+    axis_a: usize,
+    axis_b: usize,
 ) -> Vec<Option<LocalValId>> {
-    todo!()
+    // TODO: ExtractDiag/EmbedDiag will be replaced by Gather/Scatter (Tier-2)
+    match tangent_in[0] {
+        Some(dx) => {
+            let out = builder.add_op(
+                StdTensorOp::EmbedDiag { axis_a, axis_b },
+                vec![ValRef::Local(dx)],
+                OpMode::Linear {
+                    active_mask: vec![true],
+                },
+            );
+            vec![Some(out[0])]
+        }
+        None => vec![None],
+    }
 }
 
 pub fn transpose_extract_diag(
-    _builder: &mut FragmentBuilder<StdTensorOp>,
-    _cotangent_out: &[Option<LocalValId>],
-    _axis_a: usize,
-    _axis_b: usize,
+    builder: &mut FragmentBuilder<StdTensorOp>,
+    cotangent_out: &[Option<LocalValId>],
+    axis_a: usize,
+    axis_b: usize,
 ) -> Vec<Option<LocalValId>> {
-    todo!()
+    // TODO: ExtractDiag/EmbedDiag will be replaced by Gather/Scatter (Tier-2)
+    match cotangent_out[0] {
+        Some(ct) => {
+            let out = builder.add_op(
+                StdTensorOp::EmbedDiag { axis_a, axis_b },
+                vec![ValRef::Local(ct)],
+                OpMode::Linear {
+                    active_mask: vec![true],
+                },
+            );
+            vec![Some(out[0])]
+        }
+        None => vec![None],
+    }
 }
 
 pub fn transpose_embed_diag(
-    _builder: &mut FragmentBuilder<StdTensorOp>,
-    _cotangent_out: &[Option<LocalValId>],
-    _axis_a: usize,
-    _axis_b: usize,
+    builder: &mut FragmentBuilder<StdTensorOp>,
+    cotangent_out: &[Option<LocalValId>],
+    axis_a: usize,
+    axis_b: usize,
 ) -> Vec<Option<LocalValId>> {
-    todo!()
+    // TODO: ExtractDiag/EmbedDiag will be replaced by Gather/Scatter (Tier-2)
+    match cotangent_out[0] {
+        Some(ct) => {
+            let out = builder.add_op(
+                StdTensorOp::ExtractDiag { axis_a, axis_b },
+                vec![ValRef::Local(ct)],
+                OpMode::Linear {
+                    active_mask: vec![true],
+                },
+            );
+            vec![Some(out[0])]
+        }
+        None => vec![None],
+    }
 }
