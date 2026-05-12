2ec1d42eaa8fc0daccd813e3d0b994cd6313a4f3
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2026-05-12 10:35:45 +0900

    docs: add checked cpu and cuda quickstarts

---
+++ tenferro/examples/cpu_quickstart.rs
@@ -0,0 +1,15 @@
+use tenferro::{CpuBackend, Tensor};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut backend = CpuBackend::new();
+
+    let a = Tensor::from_vec(vec![2, 2], vec![1.0_f64, 3.0, 2.0, 4.0]);
+    let b = Tensor::from_vec(vec![2, 2], vec![5.0_f64, 7.0, 6.0, 8.0]);
+
+    let c = a.matmul(&b, &mut backend)?;
+
+    assert_eq!(c.shape(), &[2, 2]);
+    assert_eq!(c.as_slice::<f64>().unwrap(), &[19.0, 43.0, 22.0, 50.0]);
+
+    Ok(())
+}
