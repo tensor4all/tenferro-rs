2e5468b12903884eb9ce0377be35cbaa6c0bfacc
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2026-08-04 05:26:10 +0900

    test(storage): add p3 p9 cutover red harness

---
+++ crates/tenferro-tensor/tests/ui/storage/fail/typed_tensor_view_root_access.rs
@@ -0,0 +1,8 @@
+use tenferro_tensor::TypedTensor;
+
+fn main() {
+    let mut tensor = TypedTensor::<f64>::from_vec_col_major([1], vec![1.0]).unwrap();
+    let view = tensor.as_view_mut().unwrap();
+    let _ = tensor.shape();
+    drop(view);
+}
